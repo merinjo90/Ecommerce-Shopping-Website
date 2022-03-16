5eb24535701aaed57b83ba3057175d265774ef94
merinjo90 <merinjo90@gmail.com>
2022-03-16 12:27:18 +0530

    Password change or Forgot Password

--- ShoppingCart/shoppinglyx/app/urls.py
+++ ShoppingCart/shoppinglyx/app/urls.py
@@ -1,13 +1,12 @@
-from django.urls import path,re_path, include, reverse_lazy
+from django.urls import path, re_path, include, reverse_lazy
 # from app import views
 from .views import *
 
 from django.contrib.auth import views as auth_views
 from .forms import *
 
-
 urlpatterns = [
-    #path('', views.home),
+    # path('', views.home),
     path('', ProductView.as_view(), name='home'),
     path('product-detail/<int:pk>', ProductDetailView.as_view(), name='product-detail'),
     path('cart/', add_to_cart, name='add-to-cart'),
@@ -15,11 +14,12 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
     path('address/', address, name='address'),
     path('orders/', orders, name='orders'),
-    path('changepassword/', change_password, name='changepassword'),
     path('mobile/', mobile, name='mobile'),
     path('mobile/<slug:data>', mobile, name='mobiledata'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm),name='login'),
     # path('login/', login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',form_class=MyPasswordChangeForm),name='passwordchange'),
     path('registration/', CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', checkout, name='checkout'),
 ]
--- ShoppingCart/shoppinglyx/app/views.py
+++ ShoppingCart/shoppinglyx/app/views.py
@@ -52,9 +52,6 @@ def orders(request):
     return render(request, 'app/orders.html')
 
 
-def change_password(request):
-    return render(request, 'app/changepassword.html')
-
 
 # ------------------- Detailed Mobile data --------------------------------
 
