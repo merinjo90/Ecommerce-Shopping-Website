02038fdb86df354e68aecd356121a5c5c401f615
merinjo90 <merinjo90@gmail.com>
2022-03-16 10:46:46 +0530

    login part

--- ShoppingCart/shoppinglyx/app/forms.py
+++ ShoppingCart/shoppinglyx/app/forms.py
@@ -1,11 +1,15 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
+from django.utils.translation import gettext, gettext_lazy as _
 
+
+# --------------- Registration Form ---------------------------
 
 class CustomerRegistrationForm(UserCreationForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Confirm Password (again)',widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm Password (again)',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
     class Meta:
@@ -13,3 +17,10 @@ class CustomerRegistrationForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
         labels = {'email': 'Email'}
         widgets = {'username': forms.TextInput(attrs={'class': 'form-control'})}
+
+
+# ---------------- Login Form --------------------------
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control'}))
+    password = forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(
+        attrs={'autocomplete': 'current-password', 'class': 'form-control'}))
--- ShoppingCart/shoppinglyx/app/urls.py
+++ ShoppingCart/shoppinglyx/app/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path,re_path, include, reverse_lazy
+# from app import views
 from .views import *
 
-urlpatterns = [
-    # path('', home,name='home'),
+from django.contrib.auth import views as auth_views
+from .forms import *
 
+
+urlpatterns = [
+    #path('', views.home),
     path('', ProductView.as_view(), name='home'),
     path('product-detail/<int:pk>', ProductDetailView.as_view(), name='product-detail'),
     path('cart/', add_to_cart, name='add-to-cart'),
@@ -14,7 +18,8 @@ urlpatterns = [
     path('changepassword/', change_password, name='changepassword'),
     path('mobile/', mobile, name='mobile'),
     path('mobile/<slug:data>', mobile, name='mobiledata'),
-    path('login/', login, name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm),name='login'),
+    # path('login/', login, name='login'),
     path('registration/', CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', checkout, name='checkout'),
 ]
