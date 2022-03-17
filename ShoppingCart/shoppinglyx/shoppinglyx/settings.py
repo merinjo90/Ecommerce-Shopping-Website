7ddb9333e3aca5315f709cd6aa4311ee2ce16b69
merinjo90 <merinjo90@gmail.com>
2022-03-17 19:58:18 +0530

    change Database Sqlite to MySQL Database
        • python manage.py startapp login
        • XAMPP open
            ◦ ~/Downloads
            ◦ cd /opt/lampp/
            ◦ /opt/lampp$ sudo ./manager-linux-x64.run
            ◦ sudo service apache2 stop
            ◦ service mysql stop
        • open phpmyadmin - http://localhost/phpmyadmin
        • create new database – DB name tutorial
        • connect MYSQL
            ◦ python manage.py makemigrations
            ◦ pip install mysqlclient
            ◦ python3 manage.py migrate

--- ShoppingCart/shoppinglyx/shoppinglyx/settings.py
+++ ShoppingCart/shoppinglyx/shoppinglyx/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'shoppinglyx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
+#
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'shoppingX',
+        'ENGINE': 'mysql.connector.django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
