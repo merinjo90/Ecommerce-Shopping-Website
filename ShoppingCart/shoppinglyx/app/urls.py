686c765446d23434b2496291839cd3d0fa9b9087
merinjo90 <merinjo90@gmail.com>
2022-03-17 15:19:25 +0530

    modify urls

--- ShoppingCart/shoppinglyx/app/urls.py
+++ ShoppingCart/shoppinglyx/app/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path('pluscart/',plus_cart),
     path('minuscart/', minus_cart),
     path('removecart/', remove_cart),
-
     path('buy/', buy_now, name='buy-now'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('address/', address, name='address'),
@@ -23,23 +22,15 @@ urlpatterns = [
     path('mobile/<slug:data>', mobile, name='mobiledata'),
     path('checkout/', checkout, name='checkout'),
     path('paymentdone/', payment_done, name='paymentdone'),
-
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm),name='login'),
-    # path('login/', login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-
     path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',
                                                                   form_class=MyPasswordChangeForm,success_url='/passwordchangedone/'),name='passwordchange'),
     path('passwordchangedone/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchangedone.html'),name='passwordchangedone'),
-
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm),name='password_reset'),
-
     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'),name='password_reset_done'),
-
     path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html',
                                                                                                 form_class=MySetPasswordForm), name='password_reset_confirm'),
-
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'),name='password_reset_complete'),
-
     path('registration/', CustomerRegistrationView.as_view(), name='customerregistration'),
 ]
